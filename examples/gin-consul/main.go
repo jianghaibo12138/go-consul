85d592746466c83438b13dea0d8106ef3bea0fa5
Haibo Jiang <jianghaibo12138@gmail.com>
2021-11-10 16:20:48 +0800

    Modify example to show how to deregister check when server shutdown gracefully

--- examples/gin-consul/main.go
+++ examples/gin-consul/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jianghaibo12138/go-consul/consul_client"
 	"github.com/jianghaibo12138/go-consul/consul_client/check"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 const (
@@ -16,14 +21,14 @@ const (
 	Ssl   = false
 )
 
+var client = consul_client.ConsulClient{
+	Host:  Host,
+	Port:  Port,
+	Token: Token,
+	Ssl:   Ssl,
+}
+
 func consulRegister() {
-	fmt.Println(os.Getwd())
-	client := consul_client.ConsulClient{
-		Host:  Host,
-		Port:  Port,
-		Token: Token,
-		Ssl:   Ssl,
-	}
 	bytes := consul_client.ReadJsonConf("./examples/gin-consul/json_conf/check_register.json")
 	var payload check.RegisterPayload
 	err := json.Unmarshal(bytes, &payload)
@@ -38,15 +43,43 @@ func consulRegister() {
 }
 
 func main() {
-	//consulRegister()
+	consulRegister()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	err := r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	srv := &http.Server{
+		Addr:    "0.0.0:8080",
+		Handler: r,
+	}
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(fmt.Sprintf("listen: %s", err))
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server")
+
+	_, err := client.CheckDeRegister("gin-consul")
 	if err != nil {
 		panic(err)
 	}
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(fmt.Sprintf("Server forced to shutdown: %s", err.Error()))
+	}
+	log.Println("Server exiting")
 }
